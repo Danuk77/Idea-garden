b715688001ce8f36f836bea7f8357d694f65542a
tpeach90 <thomasopeach@btinternet.com>
2023-07-20 11:06:10 +0100

    color palette in app config

---
+++ idea-garden/app/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+  eerie_black: "#1c1d21",
+  mountbatten_pink: "#a288a6",
+  rose_quartz: "#bb9bb0",
+  silver: "#ccbcbc",
+  lavender_blush: "#f1e3e4",
+
+  black: "#000",
+  white: "#ffffff",
+  gray: "#818181",
+};
