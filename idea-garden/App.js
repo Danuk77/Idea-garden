4f244e45f26c1b561f7c0220c7a9c7e47e4a2786
tpeach90 <thomasopeach@btinternet.com>
2023-07-20 10:57:52 +0100

    front end first commit

---
+++ idea-garden/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import TestScreen from "./app/screens/TestScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TestScreen"
+          component={TestScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ idea-garden/app/screens/TestScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+function TestScreen({ navigation }) {
+  return <Text>Test screen</Text>;
+}
+
+export default TestScreen;
